49d538c9d4340cc6234251f6c0aa1b0c26d9b56e
Nikita Milko <milniklaus@gmail.com>
2020-09-08 01:28:14 +0300

    Implemented auto video call.

--- CatCam/ViewController.swift
+++ CatCam/ViewController.swift
@@ -7,13 +7,27 @@
 //
 
 import Cocoa
+import AgoraRtcKit
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var remoteVideoView: NSView!
+    @IBOutlet weak var localVideoView: NSView!
+    
+    private let appID = "5750874a9ff3449a89758408d1d546f2"
+    private let token = "0065750874a9ff3449a89758408d1d546f2IACJF+K90EBOcRfro3YQUv+L9cFcf7/Fp9OBFdZDvhDAZ4d/zXwAAAAAEACe2t6IevtXXwEAAQB6+1df"
+    private let channel = "CatcamChanel"
+    
+    private var agoraKit: AgoraRtcEngineKit?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        initializeAgoraEngine()
+        setupVideo()
+        setupLocalVideo()
+        
+        joinChannel()
     }
 
     override var representedObject: Any? {
@@ -22,6 +36,47 @@ class ViewController: NSViewController {
         }
     }
 
+    deinit {
+        agoraKit?.leaveChannel(nil)
+    }
+    
+    // MARK: Agora
+    
+    func initializeAgoraEngine() {
+       // Initialize the AgoraRtcEngineKit object.
+       agoraKit = AgoraRtcEngineKit.sharedEngine(withAppId: appID, delegate: self)
+    }
+    
+    func setupVideo() {
+        // Enable the video
+        agoraKit?.enableVideo()
+    }
+    
+    func setupLocalVideo() {
+        let videoCanvas = AgoraRtcVideoCanvas()
+        videoCanvas.uid = 0
+        videoCanvas.view = localVideoView
+        videoCanvas.renderMode = .hidden
+        // Set the local video view.
+        agoraKit?.setupLocalVideo(videoCanvas)
+    }
+    
+    func joinChannel() {
+        agoraKit?.startPreview()
+        // Join a channel with a token.
+        agoraKit?.joinChannel(byToken: token, channelId: channel, info: nil, uid: 0, joinSuccess: nil)
+        
+    }
+}
 
+extension ViewController: AgoraRtcEngineDelegate {
+    func rtcEngine(_ engine: AgoraRtcEngineKit, firstRemoteVideoDecodedOfUid uid:UInt, size:CGSize, elapsed:Int) {
+        let videoCanvas = AgoraRtcVideoCanvas()
+        videoCanvas.uid = uid
+        videoCanvas.view = remoteVideoView
+        videoCanvas.renderMode = .hidden
+        // Set the remote video view.
+        agoraKit?.setupRemoteVideo(videoCanvas)
+    }
 }
 
