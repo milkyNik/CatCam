30060b5f93b41beb68fd2a04ddb8e5f5070a58ea
Nikita Milko <milniklaus@gmail.com>
2020-09-08 00:11:25 +0300

    Initial Commit

---
+++ CatCam/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CatCam
+//
+//  Created by Nikita Milko on 08.09.2020.
+//  Copyright © 2020 Nikita Milko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CatCam/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CatCam
+//
+//  Created by Nikita Milko on 08.09.2020.
+//  Copyright © 2020 Nikita Milko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
